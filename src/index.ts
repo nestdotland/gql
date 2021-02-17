58623ab01bcdfc8fdccc6abfeb0e186ae2a2e62e
oganexon <oganexon@protonmail.com>
2021-02-18 00:29:24 +0100

    feat: :sparkles: prisma schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hey! hey!');
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
